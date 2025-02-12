21e3c76997df6930b60a3cfc9f7e83a2c65d2de8
Bemin Raafat <beminraafat@gmail.com>
2025-02-12 14:58:19 +0200

    Edit signup.js url

--- public/js/bundle/index.js
+++ public/js/bundle/index.js
@@ -22935,7 +22935,7 @@ const signup = async (data)=>{
     try {
         const res = await (0, _axiosDefault.default)({
             method: 'POST',
-            url: 'http://127.0.0.1:3000/api/v1/users/signup',
+            url: '/api/v1/users/signup',
             data
         });
         if (res.data.status === 'success') (0, _alerts.showAlert)('success', 'Account created successfully! Please verify your email.');
